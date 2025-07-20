630a4efa7356e91b17b27c02aefbedde6170ac17
diogo <bif.diogo@gmail.com>
2025-07-20 13:35:40 +0200

    move from next to react

--- server/index.ts
+++ server/index.ts
@@ -18,8 +18,6 @@ const userList: User[] = [];
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  socket.on("");
-
   socket.on("join-room", (roomId: string, userId: string) => {
     socket.join(roomId);
     socket.to(roomId).emit("user-joined", userId);
@@ -32,40 +30,11 @@ io.on("connection", (socket) => {
 });
 
 const PORT = 3001;
-
+/*
 const newRoom = new Room("1");
-newRoom.connectUser(
-  new User({
-    id: "1",
-    name: "Diogo",
-  })
-);
-
-newRoom.connectUser(
-  new User({
-    id: "2",
-    name: "Diogo 2",
-  })
-);
-
-newRoom.connectUser(
-  new User({
-    id: "3",
-    name: "Diogo 3",
-  })
-);
-
-newRoom.connectUser(
-  new User({
-    id: "4",
-    name: "Diogo 4",
-  })
-);
-
 const newGame = new Game();
 newGame.setPlayers(newRoom.getUsers());
 newGame.start();
-
 for (let i = 0; i <= 4; i++) {
   const newRound: GameRound = newGame.startNewRound();
   newGame.getPlayers().forEach((user: User) => {
@@ -76,14 +45,13 @@ for (let i = 0; i <= 4; i++) {
       );
     }
   });
-
   const roundWinner: string = Array.from(
     newRound.getRoundWhiteCardOptionsDisplay().keys()
   )[0]!;
   newRound.defineRoundWinner(roundWinner);
 }
-
 newGame.finishGame();
+*/
 
 server.listen(PORT, () => {
   console.log(`Signaling server running on http://localhost:${PORT}`);
